7d94202254827a816c6c0781f0633b2cb2b46b2e
VASS\francisco.mesa <francisco.mesa@vasscompany.com>
2024-10-11 19:29:42 +0200

    Creacion de proyecto maven.

---
+++ gestor-incidencias/src/main/java/com/tfg/incidencias/gestor_incidencias/GestorIncidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.incidencias.gestor_incidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorIncidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorIncidenciasApplication.class, args);
+	}
+
+}
---
+++ gestor-incidencias/src/test/java/com/tfg/incidencias/gestor_incidencias/GestorIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.incidencias.gestor_incidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorIncidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
